06e6c62e1eaa3ab6deded25708bf76cf11f9ceb5
longgt <longgt@aureole-it.vn>
2020-09-25 10:42:12 +0700

    feat(api): add '/api/projects' API to get project list from GitLab

--- express-server/routes/api.js
+++ express-server/routes/api.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const axios = require('axios');
 const router = express.Router();
 const keycloak = require('../keycloak-config.js').getKeycloak();
+const config = require('../config');
 
 // Anonymous route, be able to access to everyone
 router.get('/anonymous', function(req, res){
@@ -20,6 +22,25 @@ router.get('/admin', keycloak.protect('admin'), function(req, res){
 // Anonymous route, be able to access to everyone who has been granted to role 'user' or 'admin'
 router.get('/all-user', keycloak.protect(['user','admin']), function(req, res){
     res.send("Hello All User");
+});
+router.get('/projects', keycloak.protect(['user','admin']), function(req, res){
+    const idpGrant = req.session.idpGrant;
+    if (idpGrant) {
+        axios.get(`${config.hosts.gitlab}/api/v4/projects`, {
+            headers: {
+                'Authorization': `Bearer ${idpGrant.access_token}`,
+                'Accept': 'application/json'
+            }
+        }).then(idpRes => {
+            res.status(200).send(idpRes.data);
+        }).catch(err => {
+            res.status(400).send(err);
+        });
+    } else {
+        res.status(400).send({
+            message: 'Not valid GitLab user'
+        });
+    }
 });
 
 module.exports = router;
